4cc618e3b66e4436c3af5b4b3384b1e6b04ef309
Jill Short <jillian.b.short@gmail.com>
2020-07-24 23:06:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   end
   
   def self.genre_count
-    @@genre_count = {}
+    genre_count = {}
       @@genres.each do |genre|
         if genre_count[genre]
           @@genre_count[genre]+=1 
@@ -36,20 +36,9 @@ class Song
           @@genre_count[genre]=1 
         end
       end
-      @@genre_count
+      genre_count
   end
   
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
   
   def self.artist_count
     @@artist_count = {}
